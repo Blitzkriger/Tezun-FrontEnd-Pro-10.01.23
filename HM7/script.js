040a29a2bff0340024462920b099e69ee81a5380
Денис <denis@MacBook-Pro-Denis.local>
2023-01-19 23:45:25 +0200

    fix HM7

--- HM7/script.js
+++ HM7/script.js
@@ -1,23 +1,23 @@
-const mathEnteredAction = prompt('Какое математическое действие вы хотите сделать? add,sub,mult,div?');
+const mathEnteredAction = prompt('Какое математическое действие вы хотите сделать? add(+),sub(-),mult(*),div(/)?');
 const firstEnteredNumber = Number(prompt('Введите первое число.', 10));
 const secondEnteredNumber = Number(prompt('Введите второе число.', 2));
 
-if (mathEnteredAction == 'add') {
+if (mathEnteredAction == 'add' || mathEnteredAction == '+') {
 
     const addValue = firstEnteredNumber + secondEnteredNumber;
     alert(`${firstEnteredNumber} + ${secondEnteredNumber} = ${addValue}`)
 
-} else if (mathEnteredAction == 'sub') {
+} else if (mathEnteredAction == 'sub' || mathEnteredAction == '-') {
 
     const subValue = firstEnteredNumber - secondEnteredNumber;
     alert(`${firstEnteredNumber} - ${secondEnteredNumber} = ${subValue}`)
 
-} else if (mathEnteredAction == 'mult') {
+} else if (mathEnteredAction == 'mult' || mathEnteredAction == '*') {
 
     const multValue = firstEnteredNumber * secondEnteredNumber;
     alert(`${firstEnteredNumber} * ${secondEnteredNumber} = ${multValue}`)
 
-} else if (mathEnteredAction == 'div') {
+} else if (mathEnteredAction == 'div' || mathEnteredAction == '/') {
 
     const divValue = firstEnteredNumber / secondEnteredNumber;
     alert(`${firstEnteredNumber} / ${secondEnteredNumber} = ${divValue}`)
