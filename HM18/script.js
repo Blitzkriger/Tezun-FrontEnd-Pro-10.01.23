8435db072369acb6e663c415ed33acefefc2fe75
Денис <denis@MacBook-Pro-Denis.local>
2023-02-04 17:14:52 +0200

    one more fix HM18

--- HM18/script.js
+++ HM18/script.js
@@ -4,7 +4,7 @@ const array = ['q','w','e','r','t','y',1,2,3,4,5,6,7,8,9];
 
 function avarage(arr) {
 
-  let numbers = array.filter(number => {
+  let numbers = arr.filter(number => {
     if (!isNaN(number)) {
       return number;
     }
@@ -23,73 +23,73 @@ function avarage(arr) {
 let result = avarage(array);
 console.log(result)
 
-// 2.Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
+// // 2.Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
 
-const firstValue = Number(prompt('Введите первое число',2));
-const secondValue = Number(prompt('Введите второе число',2));
-const calculation = prompt('Какое действие вы хотите сделать с числами (+, -, *, /, %, ^ (степень)');
+// const firstValue = Number(prompt('Введите первое число',2));
+// const secondValue = Number(prompt('Введите второе число',2));
+// const calculation = prompt('Какое действие вы хотите сделать с числами (+, -, *, /, %, ^ (степень)');
 
 
-function doMath(x,znak,y) {
+// function doMath(x,znak,y) {
 
-  let result;
+//   let result;
 
-  switch (znak) {
-    case '+':
-      result = x + y;
-      break;
-    case '-':
-      result = x - y;
-      break;
-    case '*':
-      result = x * y;
-      break;
-    case '/':
-      result = x / y;
-      break;  
-    case '%':
-      result = x % y;
-      break;
-    case '^':
-      result = x ** y;
-      break;    
-}
-  return result;
-}
+//   switch (znak) {
+//     case '+':
+//       result = x + y;
+//       break;
+//     case '-':
+//       result = x - y;
+//       break;
+//     case '*':
+//       result = x * y;
+//       break;
+//     case '/':
+//       result = x / y;
+//       break;  
+//     case '%':
+//       result = x % y;
+//       break;
+//     case '^':
+//       result = x ** y;
+//       break;    
+// }
+//   return result;
+// }
 
-const totalResult = doMath(firstValue,calculation,secondValue);
-console.log(totalResult)
+// const totalResult = doMath(firstValue,calculation,secondValue);
+// console.log(totalResult)
 
-// 3.Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
+// // 3.Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
 
-function arrAdd() {
-  let arr = [];
-  let amount = +prompt('Введите количество массивов в основном массиве');
+// function arrAdd() {
+//   let arr = [];
+//   let amount = +prompt('Введите количество массивов в основном массиве');
 
-  for(let i = 0; i < amount; i++) {
-      arr.push([]);
+//   for(let i = 0; i < amount; i++) {
+//       arr.push([]);
 
-      let amount2 = prompt(`Введите количество елементов в ${i}-ом массиве`);
+//       let amount2 = prompt(`Введите количество елементов в ${i}-ом массиве`);
 
-      for(let j = 0; j < amount2; j++) {
-          let elem = prompt(`Введите значение элементов для данного массива`);
-          arr[i].push([elem]);
-      }
-  }
+//       for(let j = 0; j < amount2; j++) {
+//           let elem = prompt(`Введите значение элементов для данного массива`);
+//           arr[i].push([elem]);
+//       }
+//   }
 
-  console.log(arr);
-}
+//   console.log(arr);
+// }
 
-arrAdd();
+// arrAdd();
 
-// 4.Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
+// // 4.Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
 
-const word = prompt('Напишите слово');
-const deleteElement = prompt('Какие буквы хотите удалить?');
+// const word = prompt('Напишите слово');
+// const deleteElement = prompt('Какие буквы хотите удалить?');
 
-function foo(input, letters) {
-  return input.split('').map((letter) => letters.includes(letter) ? '' : letter).join('')
-}
+// function foo(input, letters) {
+//   return input.split('').map((letter) => letters.includes(letter) ? '' : letter).join('')
+// }
 
-let action = foo(word, deleteElement)
-console.log(action)
+// let action = foo(word, deleteElement)
+// console.log(action)
